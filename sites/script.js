b2cc6d322da11eff4a7f6961ef5e8e800636ec1c
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-03-06 18:23:34 +0100

    fixed tom-select; started with fast search

--- sites/script.js
+++ sites/script.js
@@ -16,5 +16,25 @@ function loadDashboardData() {
     xhttp.open("GET", "/api/stats");
     xhttp.send();
 }
-
+function insertMembersToAllUserLists() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.addEventListener("load", () => {
+        let response = JSON.parse(xhttp.responseText);
+        const dataList = document.getElementById("datalist_members");
+        for (let i = 0; i < response.length; i++) {
+        const member = response[i];
+        const option = document.createElement("option");
+        option.value = member.first_name + " " + member.second_name;
+        const textNode = document.createTextNode(option.value);
+        option.appendChild(textNode);
+        dataList.appendChild(option);
+        }
+    });
+    xhttp.open("GET", "/api/members/smaallist");
+    xhttp.setRequestHeader("Cache-Control", "no-cache, no-store, max-age=0");
+    xhttp.setRequestHeader("Expires", "Tue, 01 Jan 1980 1:00:00 GMT");
+    xhttp.setRequestHeader("Pragma", "no-cache");
+    xhttp.send();
+}
 loadDashboardData();
+insertMembersToAllUserLists();
